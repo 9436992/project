f80ef2953c2c95640066777511c803fc9142edd0
Alexander <9436992@mail.ru>
2023-01-25 20:50:28 +0600

    first lesson

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
-const category = 'toys';
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели','');
 
-console.log(`https://gallerywest/${category}/7`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
